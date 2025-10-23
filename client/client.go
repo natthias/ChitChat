8484838b5b54b659cfc5cc329708d635dbfe9d81
Matthias Nyman <natthias@proton.me>
2025-10-23 11:02:18 +0200

    initial commit

---
+++ client/client.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+// proto "ChitChat/grpc"
+// "context"
+// "log"
+
+// "google.golang.org/grpc"
+// "google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	return
+}
---
+++ server/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+// proto "ITUServer/grpc"
+// "context"
+// "log"
+// "net"
+
+// "google.golang.org/grpc"
+)
+
+func main() {
+	return
+}
